1a59f806a6bcb1e93e69235114e930e3b0dedfdd
Altanis <38045884+CoderSudaWuda@users.noreply.github.com>
2022-11-05 17:32:43 -0400

    serve content dynamically

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-require('./src/server/Server');
+// Start the client:
+const http = require('node:http');
+const fs = require('node:fs');
+
+// Load all files:
+const files = {
+    // Frontend files
+    'src/client/index.html': null,
+    'src/client/bundle.js': null,
+    'src/client/style.css': null,
+    'src/client/bincoder.js': null,
+    'src/client/img/favicon.png': null,
+    'src/client/img/background.png': null,
+
+    // SVGs:
+    'src/client/img/svgs/tanks.svg': null,
+
+    // Data files
+    'src/data/Abilities.js': null,
+    'src/data/Tanks.js': null,
+    'src/data/Moves.js': null,
+};
+
+console.log('DiepShowdown: LOADING...');
+console.log('===============');
+
+for (let [file] of Object.entries(files)) {
+    const data = fs.readFileSync(file);
+    if (file.endsWith('.svg')) file = file.replace('client/', '');
+
+    files[file] = data;
+    console.log(`Loaded file (${file}).`);
+}
+
+console.log('===============');
+
+// Serve client:
+const server = http.createServer((req, res) => {
+    // TODO: Fix how SVG is served.
+    if (req.url === '/') return res.end(files['src/client/index.html']);
+    return res.end(files[`src${req.url}`] || '404');
+});
+
+server.listen(8080, () => { 
+    console.log('Client has fully loaded! Being served at `localhost:8080`.'); 
+});
+
+require('./src/server/Server'); // Starts the server.
