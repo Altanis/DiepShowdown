44c6289ac937ce096809767cc7d6fe26509906d8
Altanis <38045884+Altanis@users.noreply.github.com>
2022-11-09 13:27:30 -0500

    add gotos in bat file

--- run.bat
+++ run.bat
@@ -1,7 +1,8 @@
 @echo off
-@cd "C:\Users\vermaa\Desktop\Coding Stuff\Ashish - pls dont touch\DiepShowdown"
+@cd "C:\Users\vermaa\Desktop\Coding Stuff\Ashish - pls dont touch\DiepShowdown" 
 @set PATH="C:\Users\vermaa\Desktop\Coding Stuff\Ashish - pls dont touch\NodeEnv";%PATH%
 @echo Started Node.js environment.
 @color 00
+:redo
 @cmd /k "node index.js"
-pause
+goto redo
