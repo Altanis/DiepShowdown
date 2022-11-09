dfaaba0bb0da76bc6e35fa6b4e495165840b636b
Altanis <38045884+Altanis@users.noreply.github.com>
2022-11-09 08:15:33 -0500

    fix run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,6 @@
 @echo off
 @cd "C:\Users\vermaa\Desktop\Coding Stuff\Ashish - pls dont touch\DiepShowdown"
-@set PATH="H:\Applications\nodejs\node-v18.12.1-win-x64";%PATH%
-@color 0a
+@set PATH="C:\Users\vermaa\Desktop\Coding Stuff\Ashish - pls dont touch\NodeEnv";%PATH%
 @echo Started Node.js environment.
 @color 00
 @cmd /k "node index.js"
